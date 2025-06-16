efd7226afa9dd8650a0e739c96f40abd8ef5fba4
Hugo <hugo.winnberg@gmail.com>
2025-06-16 19:53:19 +0200

    Update scan.h

--- ThreatSweeper/scan.h
+++ ThreatSweeper/scan.h
@@ -13,11 +13,12 @@ class Scan
 public:
     Scan();
     void recursiveIterator();
-    std::string fileToSHA256(std::string& filePath);
-    bool compareSHA256File(std::string& hashPath);
-    bool compareSHA256Str(std::string& hashStr);
+    std::string fileToSHA256(const std::string& filePath);
+    bool compareSHA256File(const std::string& hashPath);
+    bool compareSHA256Str(const std::string& hashStr);
     void load_hashes();
-
+    bool checkFileChange(const std::string& filePath, const std::string& hash);
+    std::string filePermission(const std::string& filePath);
 private:
     std::unordered_set<std::string> hash_set;
 
