6f8f111fd49af54bae040b32b5047042fa91c583
Hugo <hugo.winnberg@gmail.com>
2025-06-17 17:43:44 +0200

    Update main.cpp

--- ThreatSweeper/main.cpp
+++ ThreatSweeper/main.cpp
@@ -11,27 +11,34 @@ int main()
     DownloadHash hash;
     hash.downloadhash();
     hash.unzip();
+    
+    Window window(800,600,"ThreatSweeper");
+    window.handleEvents();
     */
 
-    //Window window(800,600,"ThreatSweeper");
-    //window.handleEvents();
 
     /*
     TO DO
     (DONE) Signature scanning
     (DONE) Real-time file system monitoring
     (DONE) Heuristic detection
-    (NOT IMPLEMENTED) Quarantine systems
+
     (PARTIALLY DONE) GUI (OpenGL integration)
-    (NOT IMPLEMENTED) Sandbox execution
+
+    (NOT IMPLEMENTED) Quarantine systems (isolate/remove threats)
+    (NOT IMPLEMENTED) Sandbox execution (run suspicious code safely)
+    (NOT IMPLEMENTED) Autorun (monitor startup apps/processes)
+    (NOT IMPLEMENTED) Memory monitor (scan RAM for threats)
+    (NOT IMPLEMENTED) Behavior analysis (track program actions)
+
+    (MAYBE IMPLEMENT) Maskin learning (detect unknown malware)
     */
 
     Scan scan;
     scan.recursiveIterator();
     //std::string path = "/home/hugo/projects/AntiVirus/.vscode/settings.json";
-    //Hash: 47aef119441d99d27b882550dcc1beafcc0eecb581ff1a59e8f6786c7ac5e1c3 Path: /home/hugo/projects/AntiVirus/.vscode/settings.json
-    //std::string hash = "47aef119441d99d27b882550dcc1beafcc0eecb581ff1a59e8f6786c7ac5e1c3";
-    //std::cout<<scan.fileToSHA256(path)<<std::endl;
+    //std::string hash = "5a08a0a77df1c63fed7063ce1f7ce936228937f28d07708a856c9dd24ca22a1d";
+    //std::cout<<scan.fileToSHA256("Trojan.exe") << std::endl;
     //std::cout<<scan.compareSHA256Str(hash)<<std::endl;
 
 }
