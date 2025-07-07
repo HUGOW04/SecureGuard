70b98e99190a971b778ffa64ad842ab95fb2ba3d
PhilipC9 <115881673+PhilipC9@users.noreply.github.com>
2025-07-08 01:37:25 +0200

    Update overview.cpp
    added overview line design

--- ThreatSweeper/overview.cpp
+++ ThreatSweeper/overview.cpp
@@ -10,7 +10,14 @@ void Overview::render()
 {
 	for (auto& widget : m_Widgets)
 	{
+
+		glColor3f(0.2196f, 0.6f, 0.4f);
+		glLineWidth(8.0f);
+
+		glRectf(260, 80, 260 + 620, 80 + 2);
+
 		widget.render();
+
 	}
 } 
 
