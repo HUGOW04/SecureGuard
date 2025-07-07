fd48968c96f3b3a2b958b63d4153ddda35a4a8d2
Hugo <hugo.winnberg@gmail.com>
2025-07-07 21:56:01 +0200

    Add files via upload

---
+++ ThreatSweeper/button.cpp
@@ -0,0 +1,58 @@
+#include "button.h"
+#include "stb_easy_font.h"
+
+
+Button::Button(float x, float y, float width, float height, std::string id, std::string buttonText, Font* font)
+    : x_Pos(x), y_Pos(y),m_Width(width),m_Height(height), m_ButtonName(id), m_Text(buttonText), m_Font(font)
+{
+}
+
+void Button::render()
+{
+    if (selected)
+    {
+        glColor3f(0.22f, 0.88f, 0.80f);
+    }
+    else
+    {
+        if (getIsHovering())
+            glColor3f(0.2196f, 0.2549f, 0.3137f); // Slightly brighter teal on hover
+        else
+            glColor3f(0.07f, 0.09f, 0.15f); // Normal teal
+
+    }
+    
+    // Draw button background
+    glPointSize(3.0f);
+    glEnable(GL_POINT_SMOOTH);
+    glBegin(GL_POINTS);
+    glVertex2f(x_Pos, y_Pos);
+    glVertex2f(x_Pos, y_Pos + m_Height);
+    glVertex2f(x_Pos + m_Width, y_Pos);
+    glVertex2f(x_Pos + m_Width, y_Pos + m_Height);
+    glEnd();
+    glDisable(GL_POINT_SMOOTH);
+
+    glLineWidth(5.0f);
+    glBegin(GL_LINES);
+    glVertex2f(x_Pos, y_Pos);
+    glVertex2f(x_Pos, y_Pos + m_Height);
+    glVertex2f(x_Pos + m_Width, y_Pos);
+    glVertex2f(x_Pos + m_Width, y_Pos + m_Height);
+    glVertex2f(x_Pos, y_Pos);
+    glVertex2f(x_Pos + m_Width, y_Pos);
+    glVertex2f(x_Pos, y_Pos + m_Height);
+    glVertex2f(x_Pos + m_Width, y_Pos + m_Height);
+    glEnd();
+
+    glRectf(x_Pos, y_Pos, x_Pos + m_Width, y_Pos + m_Height);
+
+    if (m_Font && m_Font->isValid())
+    {
+        float textX = x_Pos + 5.0f;
+        float textY = y_Pos + m_Height / 2 + 7.0f; // adjust for vertical alignment
+        m_Font->setColor(1.0f, 1.0f, 1.0f, 1.0f);
+        m_Font->render(m_Text, textX, textY, 1.0f);
+    }
+}
+
---
+++ ThreatSweeper/button.h
@@ -0,0 +1,37 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include "gl_includes.h"
+#include "font.h"
+#include <cmath>
+class Button {
+public:
+    Button(float x, float y,float width,float height, std::string id, std::string buttonText, Font* font);
+    void render();
+
+    float getX() const { return x_Pos; }
+    float getY() const { return y_Pos; }
+    float getWidth() { return m_Width; }
+    float getHeight() { return m_Height; }
+    std::string getName() const { return m_ButtonName; }
+    void setHovering(bool state) { isHovering = state; }
+    void setSelect(bool state) { selected = state; }
+    bool getIsHovering() const { return isHovering; }
+
+
+private:
+    float x_Pos;
+    float y_Pos;
+    float m_Width;
+    float m_Height;
+    std::string m_ButtonName;
+    std::string m_Text;
+    bool isHovering = false;
+    bool selected = false;
+    Font* m_Font;
+};
+
+#endif
