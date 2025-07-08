9ef2b85003337d0438b8a952722903f53a6a706f
PhilipC9 <115881673+PhilipC9@users.noreply.github.com>
2025-07-08 02:28:11 +0200

    Update overview.cpp
    added line for quick actions in overview

--- ThreatSweeper/overview.cpp
+++ ThreatSweeper/overview.cpp
@@ -8,13 +8,19 @@ Overview::Overview(std::vector<Widget>& widget)
 
 void Overview::render()
 {
+
+	// Line 1
+	glColor3f(0.2196f, 0.6f, 0.4f);
+	glLineWidth(8.0f);
+	glRectf(260, 80, 260 + 620, 80 + 2);
+	glEnd();
+
+	// Line 2
+	glColor3f(0.2196f, 0.3f, 0.4f);
+	glRectf(260, 330, 260 + 620, 330 + 2);
 	for (auto& widget : m_Widgets)
 	{
 
-		glColor3f(0.2196f, 0.6f, 0.4f);
-		glLineWidth(8.0f);
-
-		glRectf(260, 80, 260 + 620, 80 + 2);
 
 		widget.render();
 
