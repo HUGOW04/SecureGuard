7475605d4ec3a9fafebb63de522ed0afe232055b
Hugo <hugo.winnberg@gmail.com>
2025-07-08 15:21:44 +0200

    Update button.h

--- ThreatSweeper/button.h
+++ ThreatSweeper/button.h
@@ -4,12 +4,14 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include "gl_includes.h"
 #include "font.h"
 #include <cmath>
+#include "gl_includes.h"
+#include "loadimage.h"
+
 class Button {
 public:
-    Button(float x, float y,float width,float height, std::string id, std::string buttonText, Font* font);
+    Button(float x, float y,float width,float height, std::string id, std::string buttonText, Font* font, Loadimage* img);
     void render();
 
     float getX() const { return x_Pos; }
@@ -32,6 +34,7 @@ private:
     bool isHovering = false;
     bool selected = false;
     Font* m_Font;
+    Loadimage* m_Img;
 };
 
 #endif
