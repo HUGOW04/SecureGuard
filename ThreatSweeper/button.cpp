c7758a6c925d0b41b74b0cc412c5e74e3c1aadab
Hugo <hugo.winnberg@gmail.com>
2025-07-08 15:21:37 +0200

    Update button.cpp

--- ThreatSweeper/button.cpp
+++ ThreatSweeper/button.cpp
@@ -2,9 +2,10 @@
 #include "stb_easy_font.h"
 
 
-Button::Button(float x, float y, float width, float height, std::string id, std::string buttonText, Font* font)
-    : x_Pos(x), y_Pos(y),m_Width(width),m_Height(height), m_ButtonName(id), m_Text(buttonText), m_Font(font)
+Button::Button(float x, float y, float width, float height, std::string id, std::string buttonText, Font* font, Loadimage* img)
+    : x_Pos(x), y_Pos(y), m_Width(width), m_Height(height), m_ButtonName(id), m_Text(buttonText), m_Font(font), m_Img(img)
 {
+
 }
 
 void Button::render()
@@ -46,13 +47,36 @@ void Button::render()
     glEnd();
 
     glRectf(x_Pos, y_Pos, x_Pos + m_Width, y_Pos + m_Height);
-
+    
     if (m_Font && m_Font->isValid())
     {
-        float textX = x_Pos + 5.0f;
-        float textY = y_Pos + m_Height / 2 + 7.0f; // adjust for vertical alignment
+        float textX;
+        float textY;
+
+        if (m_Img)
+        {
+            float imageSize = 30.0f;
+            float padding = 10.0f;
+            float imageX = x_Pos + padding-5;
+            float imageY = y_Pos + (m_Height - imageSize) / 2.0f + 5.0f;
+            
+ 
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f); // Återställ färg så att bilden inte färgas
+            m_Img->drawImage(imageX, imageY, imageSize, imageSize);
+
+            textX = imageX + imageSize + padding;
+            textY = y_Pos + m_Height / 2 + 7.0f;
+        }
+        else
+        {
+            textX = x_Pos + 5.0f;
+            textY = y_Pos + m_Height / 2 + 7.0f;
+        }
+
         m_Font->setColor(1.0f, 1.0f, 1.0f, 1.0f);
         m_Font->render(m_Text, textX, textY, 1.0f);
     }
+
+
 }
 
