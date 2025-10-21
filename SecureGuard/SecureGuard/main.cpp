b35a98699892840bf2cf40a67a68cbaf254f30ce
Hugo <hugo.winnberg@gmail.com>
2025-10-21 12:13:46 +0200

    Update main.cpp

--- SecureGuard/SecureGuard/main.cpp
+++ SecureGuard/SecureGuard/main.cpp
@@ -3,35 +3,48 @@
 #include "window.h"
 #include "scan.h"
 
+/*
+TO DO
+(DONE) Signature scanning
+(DONE) Real-time file system monitoring
+(DONE) Heuristic detection
+(DONE) Memory monitor (scan RAM for threats)
+(DONE) Autorun (monitor startup apps/processes)
+(DONE) System tray (CPU/Memory stats)
+(DONE) FIX ALLOCATED MEMORY TO CLEANUP AT CLOSE!
+
+(PARTIALLY DONE) GUI (OpenGL integration)
+(PARTIALLY DONE) Desktop Notifications
+
+(NOT IMPLEMENTED) Network Scan/winapi rules / like wireshark :)
+
+(NOT IMPLEMENTED) FIND NEW ADDED FILES (WINDOWS API HOOK)
+(NOT IMPLEMENTED) physing mail protection/ leaked mail checker in have I been phoned website
+
+(NOT IMPLEMENTED) SCAN ALL NEW PROCESSES or APPLICATION THAT START or BY USER AND SCAN TO QUARENTINE BEFORE EXECUTED!!!
+(NOT IMPLEMENTED) Quarantine systems (isolate/remove threats) 
+(NOT IMPLEMENTED) Sandbox execution (run suspicious code safely)
+(NOT IMPLEMENTED) Behavior analysis (track program actions)
+
+(MAYBE IMPLEMENT) Maskin learning (detect unknown malware)
+*/
+
+#ifdef _DEBUG
+
 int main(int argc, char** argv)
 {
 
     Window window(900, 475, "SecureGuard");
     window.handleEvents();
 
-    /*
-    TO DO
-    (DONE) Signature scanning
-    (DONE) Real-time file system monitoring
-    (DONE) Heuristic detection
-    (DONE) Memory monitor (scan RAM for threats)
-    (DONE) Autorun (monitor startup apps/processes)
-    (DONE) System tray (CPU/Memory stats)
-    (DONE) FIX ALLOCATED MEMORY TO CLEANUP AT CLOSE!
-
-    (PARTIALLY DONE) GUI (OpenGL integration)
-    (PARTIALLY DONE) Desktop Notifications
-
-    (NOT IMPLEMENTED) Network Scan/winapi rules / like wireshark :)
-
-    (NOT IMPLEMENTED) FIND NEW ADDED FILES (WINDOWS API HOOK)
-    (NOT IMPLEMENTED) physing mail protection/ leaked mail checker in have I been phoned website
-
-    (NOT IMPLEMENTED) SCAN ALL NEW PROCESSES or APPLICATION THAT START or BY USER AND SCAN TO QUARENTINE BEFORE EXECUTED!!!
-    (NOT IMPLEMENTED) Quarantine systems (isolate/remove threats) 
-    (NOT IMPLEMENTED) Sandbox execution (run suspicious code safely)
-    (NOT IMPLEMENTED) Behavior analysis (track program actions)
-
-    (MAYBE IMPLEMENT) Maskin learning (detect unknown malware)
-    */
 }
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+
+    Window window(900, 475, "SecureGuard");
+    window.handleEvents();
+
+}
+#endif
+
